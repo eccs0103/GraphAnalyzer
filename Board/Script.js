ac64af8f3e47a6b27a83345a9bde179348ff6565
Arman <eccs0103@gmail.com>
2024-04-24 16:22:33 +0400

    0.4.10

--- Board/Script.js
+++ Board/Script.js
@@ -490,15 +490,28 @@ try {
 		 * @returns {boolean}
 		 */
 		isMesh(point) {
-			console.log(`try`);
-			const pointFrom = Object.map(this.#from.data, data => data.position);
-			const pointTo = Object.map(this.#to.data, data => data.position);
-			if (pointFrom === null || pointTo === null) return false;
-			return (
-				abs((point.y - pointFrom.y) - (point.x - pointFrom.x) * (pointTo.x - pointFrom.x) / (pointTo.y - pointFrom.y)) < EdgeEntity.#width / 2 &&
-				hypot(...point["-"](pointFrom)) > VerticeEntity.diameter &&
-				hypot(...point["-"](pointTo)) > VerticeEntity.diameter
-			);
+			const verticeFrom = this.#from.data;
+			const verticeTo = this.#to.data;
+			if (verticeFrom === null || verticeTo === null) return false;
+			const pointFrom = verticeFrom.position;
+			const pointTo = verticeTo.position;
+			
+			debugger;
+			return false;
+			// const pointFrom = Object.map(this.#from.data, data => data.position);
+			// const pointTo = Object.map(this.#to.data, data => data.position);
+			// if (pointFrom === null || pointTo === null) return false;
+			// console.log(pointFrom.toFixed(), pointTo.toFixed());
+			// try {
+			// 	const a = abs((point.y - pointFrom.y) - (point.x - pointFrom.x) * (pointTo.x - pointFrom.x) / (pointTo.y - pointFrom.y)) < EdgeEntity.#width
+			// 	return (
+			// 		a &&
+			// 		VerticeEntity.diameter / 2 < hypot(...point["-"](pointFrom)) && hypot(...point["-"](pointFrom)) < hypot(...pointFrom["-"](pointTo)) &&
+			// 		VerticeEntity.diameter / 2 < hypot(...point["-"](pointTo)) && hypot(...point["-"](pointTo)) < hypot(...pointFrom["-"](pointTo))
+			// 	);
+			// } finally {
+			// 	debugger;
+			// }
 		}
 		/** @type {EdgeEntitySocket} */
 		#from = new EdgeEntity.Socket();
